7e57b03146dcefb38e3539eb7cf08e114b674056
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-01-27 20:00:17 +0300

    Tailwind and Angular Material installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,51 @@
+// CORE
 import { Component } from '@angular/core';
+
+// COMMON MODULE
 import { CommonModule } from '@angular/common';
+
+// ROUTER
 import { RouterOutlet } from '@angular/router';
 
+// imports for material ui angular that we need later
+// import {MatButtonModule} from '@angular/material/button';
+// MatSidenavModule
+// MatGridListModule
+// MatMenuModule
+// MatButtonModule
+// MatCardModule
+// MatIconModule
+// MatExpansionModule
+// MatListModule
+// MatToolbarModule
+// MatTableModule
+// MatBadgeModule
+// MatSnackBarModule
+
 @Component({
+  // Html Selector
   selector: 'app-root',
+
+  // this component works standalone or not
   standalone: true,
+
+  // import modul
   imports: [CommonModule, RouterOutlet],
+
+  // Html Url or string to write
   templateUrl: './app.component.html',
+
+  // Style Url for scss or css files
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ecommerce';
+  // Field
+
+
+  // Constructor
+
+
+  // Methods
+
 }
