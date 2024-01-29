92412f07b2c0cc8beb1057d594c1cbc5f77ece47
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-01-29 22:22:22 +0300

    item row clearing button added

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -78,7 +78,10 @@ export class CartComponent implements OnInit {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.dataSource = this.cart.items
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items
+    })
   }
 
   // methods
@@ -87,11 +90,12 @@ export class CartComponent implements OnInit {
     return this.cartService.getTotal(items)
   }
 
+  onClearCart():void{
+    this.cartService.clearCart();
+  }
 
+  onRemoveFromCart(item: CartItem):void{
+    this.cartService.removeFromCart(item)
+  }
 
-  // getTotal(items: Array<CartItem>): number{
-  //   return items
-  //   .map(item => item.price * item.quantity)
-  //   .reduce((acc, item)=> acc + item, 0);
-  // }
 }
